977045837a3d946683eb772c0e162a3ac039459d
aryogiaziz <60728364+aryogiaziz@users.noreply.github.com>
2021-09-30 11:07:52 +0700

    Tugas Praktikum web phpmvc video 5 - view

--- app/core/App.php
+++ app/core/App.php
@@ -22,7 +22,7 @@ class App {
         if( isset($url[1]) ) {
             if( method_exists($this->controller, $url[1]) ) {
                 $this->method = $url[1];
-                unsert($url[1]);
+                unset($url[1]);
             }
         }
 
